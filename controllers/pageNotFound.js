b4fa2373fef33dba66cbd7884b915b64eb467a88
Denis Belkin <Kers001@yandex.ru>
2021-11-09 16:36:25 +0300

    fix: pagenotfound

--- controllers/pageNotFound.js
+++ controllers/pageNotFound.js
@@ -1,5 +1,3 @@
 module.exports.pageNotFound = (req, res) => {
-  if ((req.params !== 'users') || (req.params !== 'cards')) {
-    res.status(404).send({ message: 'Ресурс не найден' });
-  }
+  res.status(404).send({ message: 'Ресурс не найден' });
 };
